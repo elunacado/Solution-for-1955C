817f5fdc0afdd169a24bfebf050be863c2d32efc
Ethan Luna <ethanlunacado@gmail.com>
2024-05-22 08:45:18 -0600

    added the max workers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 
+max_attacks = 10**15
+
 def removeZeroes(ships):
     while 0 in ships:
         ships.remove(0)
@@ -24,7 +26,7 @@ def recursive_attack(numberOfAttacks, ships, attack_count=0):
             ships[-1] = 0
             removeZeroes(ships)
 
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=max_attacks) as executor:
         future = executor.submit(recursive_attack, numberOfAttacks - 1, ships, attack_count + 1)
         return future.result()
 
