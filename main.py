2a947aacdef7fdacbe6c0f5df3a6afe2ae15f772
Ethan Luna <ethanlunacado@gmail.com>
2024-05-22 09:33:08 -0600

    removed the while function

--- main.py
+++ main.py
@@ -2,30 +2,17 @@ from concurrent.futures import ThreadPoolExecutor
 
 max_attacks = 10**15
 
-def removeZeroes(ships):
-    while 0 in ships:
-        ships.remove(0)
-
 def recursive_attack(numberOfAttacks, ships, attack_count=0):
+    ships = [ship for ship in ships if ship > 0]  # Remove zeroes
+
     if numberOfAttacks == 0 or not ships:
-        if ships:
-            head = ships[0]
-            tail = ships[-1]
-        else:
-            head = tail = None
-        return head, tail
+        return (ships[0], ships[-1]) if ships else (None, None)
 
-    if attack_count % 2 == 0:  # Atacking the head
+    if attack_count % 2 == 0:  # Attack the head
         ships[0] -= 1
-        if ships[0] <= 0:
-            ships[0] = 0
-            removeZeroes(ships)
-    else:  # Atacking the tail
+    else:  # Attack the tail
         ships[-1] -= 1
-        if ships[-1] <= 0:
-            ships[-1] = 0
-            removeZeroes(ships)
-
+        
     with ThreadPoolExecutor(max_workers=max_attacks) as executor:
         future = executor.submit(recursive_attack, numberOfAttacks - 1, ships, attack_count + 1)
         return future.result()
