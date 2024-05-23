c6b93d150b3e2df14150cc8715cedd89b364718b
Ethan Luna <ethanlunacado@gmail.com>
2024-05-22 22:44:54 -0600

    .

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from concurrent.futures import ThreadPoolExecutor
 t = int(input())
 
 def recursive_attack(numberOfAttacks, ships, attack_count=0, sunken_ships=0):
-    sunken_ships += len([ship for ship in ships if ship <= 0])  # Count sunken ships
-    ships = [ship for ship in ships if ship > 0]  # Remove sunken ships
-
     if numberOfAttacks == 0 or not ships:
         return sunken_ships
 
@@ -13,6 +10,9 @@ def recursive_attack(numberOfAttacks, ships, attack_count=0, sunken_ships=0):
         ships[0] -= 1
     else:  # Attack the tail
         ships[-1] -= 1
+
+    sunken_ships += len([ship for ship in ships if ship <= 0])  # Count sunken ships
+    ships = [ship for ship in ships if ship > 0]  # Remove sunken ships
 
     with ThreadPoolExecutor(max_workers=20) as executor:
         future = executor.submit(recursive_attack, numberOfAttacks - 1, ships, attack_count + 1, sunken_ships)
